225641b63190445acaedd838ba7ebd13c0917192
Bellam Poornesh <68987273+poornes@users.noreply.github.com>
2022-07-07 20:45:19 +0530

    Add files via upload

---
+++ day4/Mul&div/script.js
@@ -0,0 +1,12 @@
+function multiply()
+	{
+	num1 = document.getElementById("number1").value;
+	num2 = document.getElementById("number2").value;
+	document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divid(){
+    num1 = document.getElementById('number1').value;
+    num2 = document.getElementById('number2').value;
+    document.getElementById('result').innerHTML = num1 / num2;
+}
