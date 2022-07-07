306646fc4d0bdad59e321f4df5bd6172d84fd294
Bellam Poornesh <68987273+poornes@users.noreply.github.com>
2022-07-07 20:50:19 +0530

    Delete Triangle directory

--- Triangle/triangle.js
+++
@@ -1,7 +0,0 @@
-let a  = 5; 
-let b  = 6; 
-let c  = 7; 
-var s = (a + b + c)/2;
-var area =  Math.sqrt(s*((s-a)*(s-b)*(s-c)));
-document.getElementById('find_area').innerHTML = area;
-// console.log(area);
